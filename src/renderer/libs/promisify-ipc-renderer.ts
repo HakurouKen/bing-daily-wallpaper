8c47e02ee7f1706d1665e7007f72c1c8f2a77e41
HakurouKen <ticshot@gmail.com>
2021-03-29 13:21:14 +0800

    feat: promisify-ipc-renderer

---
+++ src/renderer/libs/promisify-ipc-renderer.ts
@@ -0,0 +1,107 @@
+import { ipcRenderer, IpcRendererEvent } from 'electron';
+
+function uuidV4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+class TimeoutError extends Error {
+  constructor(timeout?: number, message?: string) {
+    let fullErrorMessage = `Error: request timeout`;
+    if (timeout) {
+      fullErrorMessage += ` of ${timeout}ms exceeded`;
+    }
+    if (message) {
+      fullErrorMessage += `, ${message}`;
+    }
+    super(`${fullErrorMessage}.`);
+  }
+}
+
+type IpcCallbackItem = {
+  id: string;
+  resolve: (data: any) => void;
+  reject: (error: Error) => void;
+};
+
+const listeners: {
+  [key: string]: { callbacks: IpcCallbackItem[]; listener: Function };
+} = {};
+
+function callbackResolve(
+  channel: string,
+  event: IpcRendererEvent | null,
+  response: { id: string; error: Error | null; data: any }
+) {
+  const { id, error, data } = response;
+  if (listeners[channel]) {
+    return;
+  }
+  const { callbacks } = listeners[channel];
+  const callback = callbacks.find((item) => item.id === id);
+  if (!callback) {
+    // 如果回调 ID 匹配不上，舍弃
+    console.error(`Callback id mismatch: ${id}`);
+    return;
+  }
+  listeners[channel].callbacks = listeners[channel].callbacks.filter(
+    (cb) => cb !== callback
+  );
+  const { resolve, reject } = callback;
+  if (error) {
+    reject(error);
+  } else {
+    resolve({ id, event, data });
+  }
+}
+
+function addListener(channel: string) {
+  if (listeners[channel]) {
+    return;
+  }
+  const listener = (
+    event: IpcRendererEvent,
+    response: { id: string; error: Error | null; data: any }
+  ) => {
+    callbackResolve(channel, event, response);
+  };
+  listeners[channel] = { listener, callbacks: [] };
+  ipcRenderer.on(channel, listener);
+  return listener;
+}
+
+function request(channel: string, data: any, timeout: number = 3000) {
+  let _resolve: (value: any) => void = () => {};
+  let _reject: (reason?: Error) => void = () => {};
+  const done = new Promise((resolve, reject) => {
+    _resolve = resolve;
+    _reject = reject;
+  });
+  const id = uuidV4();
+  if (!listeners[channel]) {
+    addListener(channel);
+  }
+  if (listeners[channel]) {
+    listeners[channel].callbacks.push({
+      id,
+      resolve: _resolve,
+      reject: _reject
+    });
+  }
+  if (timeout) {
+    setTimeout(() => {
+      callbackResolve(channel, null, {
+        id,
+        error: new TimeoutError(timeout, channel),
+        data: null
+      });
+    }, timeout);
+  }
+  ipcRenderer.send(channel, { id, data });
+  return done;
+}
+
+export { request };
