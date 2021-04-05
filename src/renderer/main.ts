6351a9e0dac89f76b68f8e9c2e46324a0e4bdabd
HakurouKen <leroyxyli@tencent.com>
2021-04-05 18:52:57 +0800

    feat: locale

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,8 +1,11 @@
-import { createApp } from 'vue';
-import App from '@/renderer/app.vue';
-import ElementPlus from 'element-plus';
 import 'normalize.css';
 import 'element-plus/lib/theme-chalk/index.css';
 import '@/renderer/index.css';
 
-createApp(App).use(ElementPlus).mount('#app');
+import { createApp } from 'vue';
+import App from '@/renderer/app.vue';
+import ElementPlus from 'element-plus';
+import 'dayjs/locale/zh-cn';
+import locale from 'element-plus/lib/locale/lang/zh-cn';
+
+createApp(App).use(ElementPlus, { locale }).mount('#app');
